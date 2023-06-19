a84729889f7726dc0fa56336cc6d99b074de6b72
bs rawat <119832486+bsmmeena@users.noreply.github.com>
2023-06-19 17:18:38 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+    withSonarQubeEnv(credentialsId: 'sonar-token') {
+                sh 'mvn clean package sonar:sonar' 
     }
 }
